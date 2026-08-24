ab6f8b4fbe8c2850ea36087ae0cb0ce23aaf02db
YahiaKEE <y.elsaad@yahoo.com>
2026-08-24 18:27:49 +0000

    Add Main farewell output line

---
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,8 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Lab!");
+        System.out.println("Farewell from Jenkins Lab!");
+    }
+}
