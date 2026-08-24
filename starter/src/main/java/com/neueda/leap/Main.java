ba8f07603ca110ee87fe24ee3e70ff81a53a9340
YahiaKEE <y.elsaad@yahoo.com>
2026-08-24 19:58:17 +0000

    feat: tweak farewell message for Jenkings branch test

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,7 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Lab!");
-        System.out.println("Farewell from Jenkins Lab!");
+        System.out.println("Hello Again from Jenkins Lab Part 2!");
+        
     }
 }
